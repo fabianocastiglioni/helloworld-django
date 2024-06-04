6126531f418da1a681f36366dcccb721b076392c
fabianocastiglioni <59332785+fabianocastiglioni@users.noreply.github.com>
2024-06-04 18:53:41 -0300

    update do nome

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World! Django is running!")
+    return HttpResponse("Fabiano Baptista Castiglioni - Cloud Computing & Site Reliability Engineering")
