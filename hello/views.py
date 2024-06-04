e05b40c9daf041a6133dd2988f6b83974ad02247
anderson-cruz-fernandes <anderson-cruz-fernandes@users.noreply.github.com>
2024-06-03 21:38:49 -0300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World! Django is running!")
+    return HttpResponse("Frabiano, volgo \"bíbí\" é o Kra!!!!!")
